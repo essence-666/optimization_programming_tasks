6ac9539965e4faca1fd6590c40ecaea384afebe1
essence-666 <6ejlo3epobnt@gmail.com>
2024-10-19 23:55:55 +0300

    feat(introduce mininimazion + tests)

--- Interior/main.py
+++ Interior/main.py
@@ -19,7 +19,11 @@ def compute_p(n : int, A_telda):
 
 
 
-def Interior(C : list, A : list[list], b : list, x_0 : list, alpha : float, eps : float) -> None:
+def Interior(C : list, A : list[list], b : list, x_0 : list, 
+             alpha : float = 0.5, eps : float = 0.00001, result : bool = "max") -> None:
+    
+    if (result != "max"):
+        C = [-1 * x for x in C]
     x = x_0.copy()
     count = 1
     while(1):
@@ -42,6 +46,10 @@ def Interior(C : list, A : list[list], b : list, x_0 : list, alpha : float, eps 
         x = X_star
         print("in the iteration", count, "vector is:\n ", x, "\n")
         count += 1
+
+        if count > 300:
+            print("the problem is unsolvable or try another initiail solution!")
+            return 
     
     print("the last iteration vector:\n", x)
 
@@ -49,7 +57,7 @@ def Interior(C : list, A : list[list], b : list, x_0 : list, alpha : float, eps 
     for i in range(len(C)):
         res += C[i] * x[i]
     
-    print("answer:", res)
+    print("answer:", res if result == "max" else -1 * res)
         
     
 def main():
@@ -76,10 +84,8 @@ def main():
     #     16,
     #     9
     # ]
-    # x_0 = [ 1/2, 7/2, 1, 2 ]
-    # alpha = 0.5
-    # epsilon = 0.0001
-    # Interior(C=C, A=A, b=b, x_0=x_0, alpha=alpha, eps=epsilon)
+    # x_0 = [ 1/2, 7/2, 1, 2]
+    # Interior(C=C, A=A, b=b, x_0=x_0)
     # OUTPUT
     # the last iteration vector:
     #  [
@@ -90,16 +96,38 @@ def main():
     #  ]
     #  answer: 6.999971433492358
 
-    # Test 2 not complited yet
+
+    # Test 2 
     # Problem 1.2 from ALi lab 6
-    # manimize F(x1, x2) = x1 + x2
+    # minimize F(x1, x2) = x1 + x2
     # z = -x1 - x2
     # subject to
     # 2x1 + 4x2 ≤ 16, (1)
     # x1 + 3x2 ≥ 9, (2)
     # x1 ≥ 0, (3)
     # x2 ≥ 0 (4)
-    # be provided soon....
+    # CODE 
+    # C = [1, 1, 0, 0]
+    # A = [
+    #         [2, 4, 1, 0],
+    #         [1, 3, 0, -1]
+    #     ]
+    # b = [
+    #     16,
+    #     9
+    # ]
+    # x_0 = [ 1/2, 7/2, 1, 2 ]
+    # Interior(C=C, A=A, b=b, x_0=x_0, result = "min")
+    # OUTPUT
+    # the last iteration vector:
+    #  [
+    #   1.70625437e-06 
+    #   3.00000057e+00 
+    #   3.99999431e+00 
+    #   3.41250874e-06
+    #  ]
+    # answer: 3.0000022764013425
+
 
     # Test 3
     # Problem 2 from Ali lab 6
@@ -124,9 +152,7 @@ def main():
     #     180
     # ]
     # x_0 = [1, 1, 1, 315, 174, 169]
-    # alpha = 0.5
-    # epsilon = 0.0001
-    # Interior(C=C, A=A, b=b, x_0=x_0, alpha=alpha, eps=epsilon)
+    # Interior(C=C, A=A, b=b, x_0=x_0)
     # OUTPUT
     #  the last iteration vector:
     #  [
@@ -136,8 +162,26 @@ def main():
     #   6.67605042e-05
     #   8.90140056e-06 
     #   9.60000086e+01
-    # ]
+    #  ]
     # answer: 399.9999522676773
+
+
+    # 4 not complited yet
+    # max z = 10*x1 + 20*x2
+    # subject to the constraints:
+    # -1*x1 + 2*x2 <= 15
+    # 1*x1 + 1*x2 <= 12
+    # 5*x1 + 3*x2 <= 45
+    # C = [10, 20, 0, 0, 0]
+    # A = [
+    #         [-1, 2, 1, 0, 0],
+    #         [1, 1, 0, 1, 0], 
+    #         [5, 3, 0, 0, 1]
+    #     ]
+    # b = [15, 12, 45]
+    # x_0 = [1, 1, 0, 0, 0]
+    # Interior(A=A, C=C, b=b, x_0=x_0)
+
 
 if __name__ == "__main__":
     main()
